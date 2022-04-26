6ef62ee006dd3209039890e4f5bb9253157ae1a9
AbrieEls1989 <73484157+AbrieEls1989@users.noreply.github.com>
2022-04-26 20:12:45 +0200

    Question 2. Calculating the depth

--- Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionTwo.cs
@@ -5,21 +5,38 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetPitDepth(int[] points)
         {
-            
+
             int depth = 0;
             int P = 0;
             int Q = -1;
             int R = -1;
 
+            //Loop through int array
             for (int i = 0; i < points.Length; i++)
             {
 
                 if (Q < 0 && points[i] >= points[i - 1])
-                { 
+                {
+                    Q = i - 1;
+                }
+
+                if ((Q >= 0 && R < 0))
+                {
+
+                    if (points[i] <= points[i - 1])
+                    {
+                        R = i - 1;
+                    }
+                    else
+                    {
+                        R = i;
+                    }
+
+
                 }
 
             }
-            if(depth == 0)
+            if (depth == 0)
             {
                 depth = -1;
             }
@@ -28,6 +45,6 @@ namespace Inoxico.TechnicalQuestions.Answers
 
         }
 
-       
+
     }
 }
