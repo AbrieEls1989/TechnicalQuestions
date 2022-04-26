48d6d81c24a02760a816d099c10f65517aeda365
AbrieEls1989 <73484157+AbrieEls1989@users.noreply.github.com>
2022-04-26 19:41:42 +0200

    Question 1, Build function for counting words

--- Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
+++ Inoxico.TechnicalQuestions.Answers/QuestionOne.cs
@@ -5,7 +5,30 @@ namespace Inoxico.TechnicalQuestions.Answers
     {
         public static int GetLongestSentance(string s)
         {
+
+            string[] splitVal = s.Split(".");
+
+            int maxValue = 0;
+            foreach(string sValue in splitVal)
+            {
+                int count = 0;
+                
+            }
+
             throw new NotImplementedException();
+        }
+
+        //Counting all words in sentance.
+        static int Count(string sen)
+        {
+            int wordCount = 0;
+            for (int i = 0; i < sen.Length; i++)
+            {
+                if (char.IsWhiteSpace(sen[i]) && !char.IsWhiteSpace(sen[i + 1]) && i > 0)
+                    wordCount++;
+
+                return ++wordCount;
+            }
         }
     }
 }
